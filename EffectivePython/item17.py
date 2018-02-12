2d9da90aee47f592301da09eed70c8154d0c24e2
Babis Babalis <babisbabalis@gmail.com>
2018-02-12 19:50:43 +0200

    until Chapter 3 (21/55)

---
+++ EffectivePython/item17.py
@@ -0,0 +1,12 @@
+class ReadVisits(object):
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+    def __iter__(self):
+        with open(self.data_path) as f:
+            for line in f:
+                yield int(line)
+
+visits = ReadVisits(path)
+percentages = normalize(visits)
+print(percentages)
