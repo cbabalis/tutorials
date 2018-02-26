988e798e82b29bd5f07c007d4025eb7c0c54160f
Babis Babalis <babisbabalis@gmail.com>
2018-02-26 20:14:21 +0200

    30, 31 items have been added

---
+++ EffectivePython/item30.py
@@ -0,0 +1,49 @@
+from datetime import *
+
+class Bucket(object):
+    def __init__(self, period):
+        self.period_delta = timedelta(seconds=period)
+        self.reset_time = datetime.now()
+        self.max_quota = 0
+        self.quota_consumed = 0
+
+    def __repr__(self):
+        return ('Bucket(max_quota=%d, quota_consumed=%d)' %
+                (self.max_quota, self.quota_consumed))
+
+    @property
+    def quota(self):
+        return self.max_quota - self.quota_consumed
+
+    @quota.setter
+    def quota(self, amount):
+        delta = self.max_quota - amount
+        if amount == 0:
+            # Quota being reset for a new period
+            self.quota_consumed = 0
+            self.max_quota = 0
+        elif delta < 0:
+            # Quota being filled for the new period
+            assert self.quota_consumed == 0
+            self.max_quota = amount
+        else:
+            # Quota being consumed during the period
+            assert self.max_quota >= self.quota_consumed
+            self.quota_consumed += delta
+
+
+def fill(bucket, amount):
+    now = datetime.now()
+    if now - bucket.reset_time > bucket.period_delta:
+        bucket.quota = 0
+        bucket.reset_time = now
+    bucket.quota += amount
+
+def deduct(bucket, amount):
+    now = datetime.now()
+    if now - bucket.reset_time > bucket.period_delta:
+        return False
+    if bucket.quota - amount < 0:
+        return False
+    bucket.quota -= amount
+    return True
