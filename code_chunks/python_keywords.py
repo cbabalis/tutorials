6abc84552d9cfff1b37c9e74cf9913ced6fb87be
Babis Babalis <babisbabalis@gmail.com>
2018-04-26 18:45:47 +0300

    a new python code snippet has been added.

---
+++ code_chunks/python_keywords.py
@@ -0,0 +1,42 @@
+import sys
+import fileinput
+
+class Keywords:
+    """ This is a class containing keywords.
+    """
+
+    def __init__(self):
+        pass
+
+
+    def replace_line_in_file(self, a_file, keyword_to_find, replace_line):
+        """ This method opens a file and searches for a keyword in it and
+            it replaces it with the replace_line string.
+
+            :param string a_file: [mandatory] is the file to be searched
+                and updated.
+            :param string keyword_to_find: [mandatory] the keyword to be
+                found inside the file.
+            :param string replace_line: [mandatory] the replace line.
+        """
+        for line in fileinput.input(a_file, inplace=1):
+            if keyword_to_find in line:
+                line = replace_line + '\n'
+            sys.stdout.write(line)
+
+    def replace_value(self, a_file, key, replace_value, delimiter='='):
+        """ Doc here
+        """
+        text = ''
+        with open(a_file, 'r') as f:
+            for line in f.readlines():
+                if key in line:
+                    temp_line = line.split(delimiter)[0]
+                    line = temp_line + delimiter + str(replace_value) +'\n'
+                text += line
+
+        if not text:
+            print "No such key could be found..."
+
+        with open(a_file, 'w') as f:
+            f.write(text)
---
+++ code_chunks/run.py
@@ -0,0 +1,8 @@
+from python_keywords import Keywords
+
+def main():
+    x = Keywords()
+    x.replace_value('test.txt', 'bob', 30, '=')
+
+if __name__ == '__main__':
+    main()
